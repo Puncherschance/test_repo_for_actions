4b245c0b7dbda67d6036ba5bdf947f004f619519
Vladislav Krasheninnikov <vladislav.krash187@gmail.com>
2024-06-05 08:47:32 +0400

    test

--- tests.py
+++ tests.py
@@ -1,25 +1,28 @@
 import allure
 
-@allure.step('test 1')
-def test_number_1_passed():
-    assert 1 + 1 == 2
 
-@allure.step('test 2')
-def test_number_2_passed():
-    assert 1 + 1 == 2
+class TestsSimple:
 
-@allure.step('test 3')
-def test_number_3_passed():
-    assert 1 + 1 == 2
+    @allure.title('test 1')
+    def test_number_1_passed(self):
+        assert 1 + 1 == 2
 
-@allure.step('test 4')
-def test_number_4_passed():
-    assert 1 + 1 == 2
+    @allure.title('test 2')
+    def test_number_2_passed(self):
+        assert 1 + 1 == 2
 
-@allure.step('test 5')
-def test_number_5_failed():
-    assert 1 + 1 == 2
+    @allure.title('test 3')
+    def test_number_3_passed(self):
+        assert 1 + 1 == 2
 
-@allure.step('test 6')
-def test_number_6_failed():
-    assert 1 + 1 == 2
+    @allure.title('test 4')
+    def test_number_4_passed(self):
+        assert 1 + 1 == 2
+
+    @allure.title('test 5')
+    def test_number_5_failed(self):
+        assert 1 + 1 == 3
+
+    @allure.title('test 6')
+    def test_number_6_failed(self):
+        assert 1 + 1 == 3
